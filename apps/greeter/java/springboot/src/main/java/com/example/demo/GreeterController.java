b58311fabb6e074ee0879a3b784db3d6bc396b88
Vincent Demeester <vincent@sbr.pm>
2019-10-22 10:33:16 +0200

    devfest
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- apps/greeter/java/springboot/src/main/java/com/example/demo/GreeterController.java
+++ apps/greeter/java/springboot/src/main/java/com/example/demo/GreeterController.java
@@ -17,7 +17,7 @@ public class GreeterController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GreeterController.class);
 
-    private static final String RESPONSE_STRING_FORMAT = "%s %s greeter => '%s' : %d\n";
+    private static final String RESPONSE_STRING_FORMAT = "%s %s greeter devfest => '%s' : %d\n";
 
     private final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
 
